52abb004ec873c2c922ff971527f4db9d6dd425e
Ravana <47528708+ravana69@users.noreply.github.com>
2019-07-26 12:16:57 +0530

    Update uniborg.py

--- uniborg/uniborg.py
+++ uniborg/uniborg.py
@@ -32,7 +32,7 @@ class Uniborg(TelegramClient):
         kwargs = {
             "api_id": 6,
             "api_hash": "eb06d4abfb49dc3eeb1aeb98ae0f581e",
-            "device_model": "Iphone XI",
+            "device_model": "Iphone XI (Clone)",
             "app_version": "@r4v4n4 9.0.9",
             "lang_code": "ml",
             **kwargs
